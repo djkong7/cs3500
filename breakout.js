c4c764ad83fb46bab2557144cc2a758cfa6eb246
djkong7 <dannyj.kiel@gmail.com>
2017-04-02 13:03:34 -0400

    Moved code around

--- breakout.js
+++ breakout.js
@@ -23,6 +23,22 @@ Brick.prototype.draw = function(c) {
     c.stroke();
 };
 
+function Ball(x,y,vx,vy,r,c) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.radius = r;
+    this.color = c;
+}
+
+Ball.prototype.draw = function(c){
+    c.beginPath();
+    c.arc(this.x,this.y,this.radius,0,2*Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+};
+
 function drawCanvas(now) {
     var w = canvas.width;
     var h = canvas.height;
@@ -118,21 +134,6 @@ function toggleFullscreen() {
   }
 }
 
-function Ball(x,y,vx,vy,r,c) {
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-    this.radius = r;
-    this.color = c;
-}
-
-Ball.prototype.draw = function(c){
-    c.beginPath();
-    c.arc(this.x,this.y,this.radius,0,2*Math.PI);
-    c.fillStyle = this.color;
-    c.fill();
-};
 
 
 
