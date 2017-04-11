bded9eede52b581e80373d963f96aa68fef814e9
djkong7 <dannyj.kiel@gmail.com>
2017-04-11 15:23:22 -0400

    Removed Modules
    Removed the node_modules folder b/c they must be individually installed
    and added a gitignore file to ignore them.

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+const PORT = 3000;
+
 // Create an Express variable
-var express = require('express');
+const express = require('express');
 
 // Create a new Express application
-var app = express();
+const app = express();
 
 // Create an http server with Node's HTTP module. 
-// Pass it the Express application, and listen on port 3000. 
-var server = require('http').createServer(app);
+const server = require('http').Server(app);
 
+// Setup default directory for client (used for src='' in html file)
 app.use(express.static(__dirname));
 
+// Send the html file to the client
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/breakout.html');
 });
 
-app.listen(3000, function () {
-    console.log('listening on *:3000');
+// Instantiate Socket.IO hand have it listen on the Express/HTTP server
+const io = require('socket.io').listen(server);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
 });
 
-// Instantiate Socket.IO hand have it listen on the Express/HTTP server
-var io = require('socket.io').listen(server);
+// Listen on port 3000
+app.listen(PORT, function () {
+    //console.log('listening on *:3000');
+});
